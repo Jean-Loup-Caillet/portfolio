115e020b2d8063033ab05a816c4394b6d45c887a
Jean-Loup Caillet <Jean-Loup.Caillet@pm.me>
2025-06-03 21:02:25 +0200

    misunderstood base naming

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://Jean-Loup-Caillet.github.io",
-  base: "/github-page",
+  base: "/porfolio",
 });
